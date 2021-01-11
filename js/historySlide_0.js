7027e52622be472b086d1b213227c95b2fc6778d
Jade-SY <jadesy90@gmail.com>
2021-01-11 10:13:05 +0900

    Fix tablet history slide

--- js/historySlide_0.js
+++ js/historySlide_0.js
@@ -42,41 +42,49 @@ hsBtnNext.addEventListener('click', historySlideToNext)
 //태블릿 사이즈 이하 슬라이드
 
 //변수들
-let slideLength = historyPage.length
+let slideLength = slideConWrap.children.length
+let currIdx = 0
 let autoPlayInit
+const conWidth = slideConWrap.children[0].clientWidth
 
 function appendSlide(){
   const slideClone_1st = slideConWrap.firstElementChild.cloneNode(true)
   const slideClone_2nd = slideConWrap.children[1].cloneNode(true)
   const slideClone_3rd = slideConWrap.children[2].cloneNode(true)
 
+  const slideClone_Last = slideConWrap.lastElementChild.cloneNode(true)
+
   slideConWrap.append(slideClone_1st, slideClone_2nd, slideClone_3rd)
+  slideConWrap.insertBefore(slideClone_Last, slideConWrap.firstElementChild)
 }
 
 
 // YEAR INDEX STYLE CHANGE
-function change_HistoryPageClass(){
-  for(let i = 0; i < slideLength; i++){
-    historyPage[i].classList.remove('on');
+function change_HistoryPageClass(){ 
+  if(currIdx >= slideLength){
+    historyPage[0].classList.add('on')
+    historyPage[slideLength - 1 ].classList.remove('on')
+  }else{
+    for(let i = 0; i < slideLength; i++){
+      historyPage[i].classList.remove('on');
+    }
+    historyPage[currIdx].classList.add('on');
   }
-  historyPage[slideIdx].classList.add('on');
 }
 
 
 // TABLET SIZE SLIDE SHOW
-function tabletSizeSlideShow(i) {
-  const conWidth = slideConWrap.children[0].clientWidth
+function tabletSizeSlideShow() {
 
-  slideIdx += i
-  console.log(slideIdx, slideLength);
+  currIdx ++
+  console.log('curr '+ currIdx, slideLength)
+  slideConWrap.style = `transform: translateX(-${ (conWidth + 30) * (currIdx + 1)}px); transition: all .7s;`
 
-  if(slideIdx > slideLength ) {    
-    slideIdx = 0;
-    slideConWrap.style = `transform: translateX(0); transition: 0s;` 
-  } else {
-    slideConWrap.style = `transform: translateX(-${ (conWidth + 30) * slideIdx}px); transition: all .7s;`
-  }
-  change_HistoryPageClass();
+    if(currIdx > slideLength){
+      currIdx = 0;
+      slideConWrap.style = `transform: translateX(-${ (conWidth + 30) }px); transition: 0s;` 
+    }
+  change_HistoryPageClass(); 
 }
 
 
@@ -87,20 +95,22 @@ function tabletSizeSlideShow(i) {
 
 
 // YEAR INDEX CLICK
-historyPage.forEach( (el, i) => {
+historyPage.forEach( (el, i) => { 
   el.addEventListener('click', () => { 
-    console.log(i)
-    
-    tabletSizeSlideShow(i);
-  
     // 클릭시 AUTO PLAY 멈춤, 재시작
+    currIdx = i
     clearInterval(autoPlayInit)
+    change_HistoryPageClass();
+
+    slideConWrap.style = `transform: translateX(-${ (conWidth + 30) * (currIdx + 1)}px); transition: all .7s;`
+    
+    console.log(i, 'click, curr '+ currIdx)
     setTimeout(() => {
-      autoPlayInit = setInterval(tabletSizeSlideShow, 3000, 1)
+      autoPlayInit = setInterval(tabletSizeSlideShow, 2000, 1)
 
       console.log('재시작')
 
-    }, 500)
+    }, 600)
   })
 });
 
@@ -112,13 +122,14 @@ function matchWidth() {
   if(window.matchMedia('(max-width: 1024px)').matches && autoPlayState === false){
     console.log("1024px 이하 match")
     appendSlide()
-    
+
+    slideConWrap.style = `transform: translateX(-${ (conWidth + 30) }px);`
     historyPage[0].classList.add('on')
-    autoPlayInit = setInterval(tabletSizeSlideShow, 3000, 1)
+    autoPlayInit = setInterval(tabletSizeSlideShow, 2000, 1)
     autoPlayState = true
     console.log("오토플레이 상태:" + autoPlayState); 
   }else{
-    slideIdx = 0
+    currIdx = 0
     historyPage[0].classList.remove('on')
     autoPlayState = false
   }
@@ -129,7 +140,7 @@ matchWidth() //DOMContentLoaded 역할
 
 window.addEventListener('resize', () => {
   if(window.innerWidth > 1024){
-    slideIdx = 0
+    currIdx = 0
 
     clearInterval(autoPlayInit)
     autoPlayState = false
@@ -138,7 +149,7 @@ window.addEventListener('resize', () => {
 
   if (window.innerWidth <= 1024 && autoPlayState === false) {
     console.log("한번씩만 작동")
-    slideIdx = 0
+    currIdx = 0
 
     historySlide.classList.remove('on') 
     matchWidth()
