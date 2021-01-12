e4ad0fadcf5c5079f8f622e7494615e0b76aff95
Jade-SY <jadesy90@gmail.com>
2021-01-13 05:04:37 +0900

    Fix history slide function

--- js/historySlide_0.js
+++ js/historySlide_0.js
@@ -45,7 +45,10 @@ hsBtnNext.addEventListener('click', historySlideToNext)
 let slideLength = slideConWrap.children.length
 let currIdx = 0
 let autoPlayInit
-const conWidth = slideConWrap.children[0].clientWidth
+let autoPlayState = false; //오토플레이 상태: false = 꺼짐
+let isAppend = false
+
+
 
 function appendSlide(){
   const slideClone_1st = slideConWrap.firstElementChild.cloneNode(true)
@@ -57,6 +60,34 @@ function appendSlide(){
   slideConWrap.append(slideClone_1st, slideClone_2nd, slideClone_3rd)
   slideConWrap.insertBefore(slideClone_Last, slideConWrap.firstElementChild)
 }
+
+function removeSlide(){
+  const slideCon = document.querySelectorAll('.slide-contents')
+  // slideConArr = Array(slideConWrap.children)
+  // slideConArr.pop()
+  // console.log(slideConArr)
+  if(slideConWrap.children.length > 13){
+    // slideCon[0].remove()
+    // slideCon[14].remove()
+    // slideCon[15].remove()
+    // slideCon[16].remove()
+    // isAppend = false
+  }
+}
+
+function appendSlide_Init(){
+  if(window.innerWidth <= 1024 && isAppend === false){
+    isAppend = true
+    appendSlide()
+  }else{
+    removeSlide()
+    isAppend = false
+  }
+}
+
+/* resize이벤트에 연동되면 사이즈가 변동될때마다 반복되어 실행되는 것을 막기 위해
+isAppend 불린 조건 추가
+*/
 
 
 // YEAR INDEX STYLE CHANGE
@@ -82,14 +113,13 @@ function change_HistoryPageClass(){
 
 // TABLET SIZE SLIDE SHOW
 function tabletSizeSlideShow() {
-
   currIdx ++
-  console.log('curr '+ currIdx, slideLength)
-  slideConWrap.style = `transform: translateX(-${ (conWidth + 30) * (currIdx + 1)}px); transition: all .7s;`
+  console.log('currIdx='+ currIdx, slideLength)
+  slideConWrap.style = `transform: translateX(-${ 350 * (currIdx + 1)}px); transition: all .7s;`
 
     if(currIdx > slideLength){
       currIdx = 0;
-      slideConWrap.style = `transform: translateX(-${ (conWidth + 30) }px); transition: 0s;` 
+      slideConWrap.style = `transform: translateX(-350px); transition: 0s;` 
     }
   change_HistoryPageClass(); 
 }
@@ -97,44 +127,28 @@ function tabletSizeSlideShow() {
 
 // YEAR INDEX CLICK
 historyPage.forEach( (el, i) => { 
+
   el.addEventListener('click', () => { 
     // 클릭시 AUTO PLAY 멈춤, 재시작
     currIdx = i
     clearInterval(autoPlayInit)
     change_HistoryPageClass();
 
-    slideConWrap.style = `transform: translateX(-${ (conWidth + 30) * (currIdx + 1)}px); transition: all .7s;`
+    slideConWrap.style = `transform: translateX(-${ 350 * (currIdx + 1)}px); transition: all .7s;`
     
-    console.log(i, 'click, curr '+ currIdx)
+    console.log(i, 'click, currIdx='+ currIdx + '재시작')
     setTimeout(() => {
-      autoPlayInit = setInterval(tabletSizeSlideShow, 2000, 1)
-
-      console.log('재시작')
-
+      autoPlayInit = setInterval(tabletSizeSlideShow, 2000)
     }, 600)
   })
 });
 
-
-// 미디어쿼리처럼 사용가능
-let autoPlayState = false; //오토플레이 상태: false = 꺼짐
-let isAppend = false
-
-function appendSlide_Init(){
-  if(window.innerWidth <= 1024 && isAppend === false){
-    isAppend = true
-    appendSlide()
-  }
-}
-appendSlide_Init()
-
 function matchWidth() {
   if(window.matchMedia('(max-width: 1024px)').matches && autoPlayState === false){
     console.log("1024px 이하 match")
-    
-    slideConWrap.style = `transform: translateX(-${ (conWidth + 30) }px);`
+    slideConWrap.style = `transform: translateX(-350px);`
     historyPage[0].classList.add('on')
-    autoPlayInit = setInterval(tabletSizeSlideShow, 2000, 1)
+    autoPlayInit = setInterval(tabletSizeSlideShow, 2000)
     autoPlayState = true
     console.log("오토플레이 상태:" + autoPlayState); 
   }else{
@@ -142,6 +156,7 @@ function matchWidth() {
     historyPage[0].classList.remove('on')
     autoPlayState = false
   }
+  appendSlide_Init() 
 }
 
 matchWidth() //DOMContentLoaded 역할
@@ -150,9 +165,11 @@ matchWidth() //DOMContentLoaded 역할
 window.addEventListener('resize', () => {
   if(window.innerWidth > 1024){
     currIdx = 0
-
+    slideConWrap.style = `transform: translateX(0px);`
     clearInterval(autoPlayInit)
     autoPlayState = false
+    historyPage.forEach(el => el.classList.remove('on')
+    )
     console.log("오토플레이 상태:" + autoPlayState) //오토플레이 상태: false = 꺼짐
   }
 
@@ -163,5 +180,5 @@ window.addEventListener('resize', () => {
     historySlide.classList.remove('on') 
     matchWidth()
   }
-  appendSlide_Init()
 })
+
